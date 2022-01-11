e7110f8bc2da1269afc5d7d1fa0dbddbabf9f1d6
velkyam <velkyam@hotmail.com>
2022-01-10 21:30:11 -0800

    starter files and api fetch

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var word = "program"
+var requestUrl = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'+ word +'?key=02dc8eac-3c8b-4ee7-98f5-c159035179dd';
+var requestUrl2= 'https://excuser.herokuapp.com/v1/excuse/3'
+
+
+//random excuse, to add category and number -> add {category}/{number} to the url
+fetch(requestUrl2)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data)
+});
+
+//synonyms -> data[1].meta.syns
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    });
