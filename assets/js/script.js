c219461d146800f939d2d72bf353de864bcbd29a
Lorena Zuniga <lzuniga1315@gmail.com>
2022-01-11 13:17:04 -0800

    created var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var word = "program"
 var requestUrl = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'+ word +'?key=02dc8eac-3c8b-4ee7-98f5-c159035179dd';
 var requestUrl2= 'https://excuser.herokuapp.com/v1/excuse/3'
+var cars = ["Good morning", "Good afternoon", "Good evening" , "Hey"];
 
 
 //random excuse, to add category and number -> add {category}/{number} to the url
